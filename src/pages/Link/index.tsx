16e75dd4c438b398a62819b49887fbc95d9397be
Oori Mark <oorimark@gmail.com>
2024-01-09 15:58:46 +0100

    fixed merge conflict

--- src/pages/Link/index.tsx
+++ src/pages/Link/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { CreateLinkPage } from './create'
-import { JonLinkPage } from './join'
+import React from "react";
+import CreateLinkPage from "./create";
+import { JonLinkPage } from "./join";
 
 export const LinkPage = () => {
-    return (
-        <React.Fragment>
-            <CreateLinkPage />
-            <JonLinkPage />
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <CreateLinkPage />
+      <JonLinkPage />
+    </React.Fragment>
+  );
+};
